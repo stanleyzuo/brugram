aae0f5fb189127a6b42172d435ea0a4fc7b73584
oscarw3 <oscarw3@gmail.com>
2015-05-30 14:18:52 +0200

    basic functionality of uploading images and views working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,22 +1,47 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 
-  def edit 
+  def edit
+    @photo = Photo.find(params[:id]) 
   end
 
   def update
+      @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+      redirect_to @photo
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @photo = Photo.find(params[:id])
+   @photo.destroy
+    redirect_to photos_path
   end
 
   def create 
+    @photo = Photo.create(photo_params)
+       if @photo.save
+        redirect_to @photo
+      else
+        render 'new'
+     end
   end
 
   def new 
+    @photo = Photo.new
+  end
+
+  private
+  def photo_params
+      params.require(:photo).permit(:avatar, :caption)
   end
 end
